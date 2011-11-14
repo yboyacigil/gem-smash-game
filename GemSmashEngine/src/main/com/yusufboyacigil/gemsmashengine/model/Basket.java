21fdbc3ce5c5cb78278db32a2fc458995c1208a3
yboyacigil <yboyacigil@gmail.com>
2011-11-14 15:34:56 +0200

    Implemented gem picking

---
+++ GemSmashEngine/src/main/com/yusufboyacigil/gemsmashengine/model/Basket.java
@@ -0,0 +1,61 @@
+package com.yusufboyacigil.gemsmashengine.model;
+
+/**
+ * Represents basket where gems collected in.
+ * @author yboyacigil
+ */
+public class Basket {
+
+	private int gem;
+	private int numGems;
+	
+	public void setGem(int gem) {
+		if (this.gem != 0) {
+			throw new IllegalStateException("Gem already set for this basket (" + gem + ")");
+		}
+		this.gem = gem;
+	}
+	
+	public int gem() {
+		return this.gem;
+	}
+	
+	public int numGems() {
+		return this.numGems;
+	}
+	
+	public boolean put(int gem, int c) {
+		if (this.gem != gem) {
+			return false;
+		}
+		numGems += c;
+		return true;
+	}
+	
+	public int take(int c) {
+		if (numGems == 0) {
+			return 0;
+		}
+		if (c > numGems) {
+			int actualNumGems = numGems;
+			numGems = 0;
+			return actualNumGems;
+		}
+		numGems -= c;
+		return c;
+	}
+	
+	public boolean isEmpty() {
+		return numGems == 0;
+	}
+	
+	public boolean cannotPut(int gem) {
+		return this.gem != gem;
+	}
+
+	@Override
+	public String toString() {
+		return "[g:" + gem + ", #:" + numGems + "]";
+	}
+
+}
